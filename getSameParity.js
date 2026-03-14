7fff025809cfd9043ccb361545df2c2e9a1274dc
Васисуалий Тутыкин <vttk101@mail.ru>
2026-03-14 18:19:22 +0500

    lesson9

--- getSameParity.js
+++ getSameParity.js
@@ -1,17 +1,42 @@
+// Реализуйте функцию getSameParity(), которая принимает на вход массив чисел и возвращает новый,
+//состоящий из элементов, у которых такая же чётность, как и у первого элемента входного массива.
+//Экспортируйте функцию по умолчанию.
+
+// Моё решение
 const getSameParity = (arr) => {
     if (arr.lenght === 0) {
         return []
     }
-    const result = []
+    const result1 = []
+    const result2 = []
     for (const item of arr) {
         if (item % 2 !== 0) {
-            result.push(item)
-        } 
+            result1.push(item)
+        } else if (item % 2 === 0){
+            result2.push(item)
+        }
     }
-    return result
+    return (arr[0] % 2 !== 0) ? result1 : result2
 }
 
-const cool = [1, 2, 3]
-const cool1 = [1, 2, 8]
-const cool2 = [2, 2, 8]
-console.log(getSameParity (cool))
+export default getSameParity
+
+//Решение учителя
+const getSameParity = (coll) => {
+  if (coll.length === 0) {
+    return []
+  }
+
+  const result = []
+  const remainder = Math.abs(coll[0] % 2)
+
+  for (const item of coll) {
+    if (Math.abs(item % 2) === remainder) {
+      result.push(item)
+    }
+  }
+
+  return result
+}
+
+export default getSameParity

27277978d4d9110072efddba9226742a77287533
optzap-m@mail.ru <mihail@DESKTOP-I1GGCGG.localdomain>
2026-03-19 10:55:34 +0500

    lesson12

--- buildDefinitionList.js
+++ buildDefinitionList.js
@@ -1,23 +1,41 @@
-const definitions = [
-  ['definition1', 'description1'],
-  ['definition2', 'description2'],
-]
+//Реализуйте функцию buildDefinitionList(), которая генерирует HTML список определений 
+//(теги <dl>, <dt> и <dd>) и возвращает получившуюся строку. 
+//При отсутствии элементов в массиве функция возвращает пустую строку. Экспортируйте функцию по умолчанию.
 
-const definitions1 = [
-  ['Блямба', 'Выпуклость, утолщения на поверхности чего-либо'],
-  ['Бобр', 'Животное из отряда грызунов'],
-]
-
+// Моё решение
 const buildDefinitionList = (coll) => {
+  if (coll.length === 0)
+   return ''
   const parts = []
   for (const item of coll) {
     parts.push(`<dt>${item[0]}</dt><dd>${item[1]}</dd>`)
   }
   
-  const innerValue = parts.join( )
+  const innerValue = parts.join('')
   
   const result = `<dl>${innerValue}</dl>`
   return result
 }
+export default buildDefinitionList
 
-console.log(buildDefinitionList(definitions1))
+// Решение учителя
+const buildDefinitionList = (definitions) => {
+  if (definitions.length === 0) {
+    return ''
+  }
+
+  const parts = []
+
+  for (const definition of definitions) {
+    const name = definition[0]
+    const description = definition[1]
+    parts.push(`<dt>${name}</dt><dd>${description}</dd>`)
+  }
+
+  const innerValue = parts.join('')
+  const result = `<dl>${innerValue}</dl>`
+
+  return result
+}
+
+export default buildDefinitionList

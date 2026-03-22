313b0da287a2c5e1fa73fb638a5942281d74629c
Васисуалий Тутыкин <vttk101@mail.ru>
2026-03-22 23:33:52 +0500

    //1

--- makeCensored.js
+++ makeCensored.js
@@ -16,3 +16,45 @@ const makeCensored = (sentence, stoparr) => {
 }
 
 console.log(makeCensored(sentence, stoparr))
+
+// версия2
+
+const makeCensored = (sentence, stoparr) => {
+    
+    const separator = ' '
+    const sentenceArr = sentence.split(separator)
+    
+//    console.log(sentenceArr)
+    
+    let result = []
+    const krakozybra = '$#%!'
+    
+    for (const word of sentenceArr) {
+        (word.includes(stoparr[0]) === false) ? result.push(word) : result.push(krakozybra)
+        console.log(result)
+    }
+
+    return result.join(separator)
+}
+
+console.log(makeCensored(sentence, stoparr))
+
+const makeCensored = (sentence, stoparr) => {
+    
+    const separator = ' '
+    const sentenceArr = sentence.split(separator)
+    
+    for (const stopWord of stoparr) {
+        let stop = stopWord
+    }
+    
+    let result = []
+    
+    for (const word of sentenceArr) {
+        (word.includes(stoparr[1]) === false) ? result.push(word) : result.push('$#%!')
+    }
+
+    return result.join(separator)
+}
+
+console.log(makeCensored(sentence, stoparr))
